f77e6038091f85a03fcf4146135228a6932e8c08
[asterofspace] <[asterofspace@hotmail.com]>
2016-04-13 21:26:28 -0700

    delted old index.php with carrierlist

--- smstest/index.php
+++ smstest/index.php
@@ -1,1 +1,20 @@
- 
+<?php
+
+$usrCarrier  = $_POST['usrCarrier'];
+$usrCell 	 = 	$_POST['usrCell'];
+$carrierList = array($usrCarrier => $usrCell );
+
+/* AllTel: number@message.alltel.com
+AT&T: number@txt.att.net
+Boost Mobile: number@myboostmobile.com
+Cricket: number@sms.mycricket.com
+Nextel: number@messaging.nextel.com
+Qwest: number@qwestmp.com
+Sprint: number@messaging.sprintpcs.com
+T-Mobile: number@tmomail.net
+Tracfone: number@mmst5.tracfone.com
+U.S. Cellular: number@email.uscc.net
+Verizon: number@vtext.com
+Virgin Mobile: number@vmobl.com
+*/
+ ?>
